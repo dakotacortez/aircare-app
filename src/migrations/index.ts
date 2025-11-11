591ed7af0e832164020b5af0c55d236bcd931473
dakotacortez <135252857+dakotacortez@users.noreply.github.com>
2025-11-10 22:50:57 -0500

    Remove manual protocols migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,8 +1,1 @@
-import * as migration_20250214_000000_protocols from './20250214_000000_protocols'
-
-export const migrations = [
-  {
-    up: migration_20250214_000000_protocols.up,
-    down: migration_20250214_000000_protocols.down,
-  },
-]
+export const migrations = []
